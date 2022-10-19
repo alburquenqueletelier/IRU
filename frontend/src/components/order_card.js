7ea5aedd8796af68e292648851c4acb8f311dd61
Bryan Alburquenque <alburquenque.letelier@gmail.com>
2022-10-19 17:28:06 -0300

    Fixed productcard. Se corrige vista order y ordercard.

--- frontend/src/components/order_card.js
+++ frontend/src/components/order_card.js
@@ -1,13 +1,14 @@
 import React, { useState, useContext } from "react";
 import { Context } from "../store/appContext";
 import PropTypes from "prop-types";
+import "../styles/ordercard.css";
 
 export const OrderCard = (props) => {
-    const {actions} = useContext(Context);
+    const { actions } = useContext(Context);
     const [amount, setAmount] = useState(props.amount);
 
     const changeAmount = (value, id) => {
-        if (value > 0 && value < 10){
+        if (value > 0 && value < 10) {
             // Falta cambiar el valor del store cuando cambia acá 
             actions.editOrder(props.type, props.id, value);
             return setAmount(value);
@@ -15,20 +16,25 @@ export const OrderCard = (props) => {
     };
 
     return (
-        <div className="card mb-3" style={{maxWidth: "540px"}}>
+        <div className="card mb-3" style={{ maxWidth: "540px" }}>
             <div className="row g-0">
                 <div className="col-md-4">
-                    <img src={props.image} className="img-fluid rounded-start" alt="..." />
+                    <div className="img-container">
+                    <img src={props.image} className="img-card" alt="..." />
+
+                    </div>
                 </div>
-                <div className="col-md-8">
-                    <div className="card-body">
+                <div className="col-md-8 d-flex flex-column justify-content-between">
+                    <div className="card-body justify-content-around d-flex flex-column">
                         <h5 className="card-title">{props.title}</h5>
-                        <p className="card-text"><strong className="text-danger">Valor: </strong> {props.price}</p>
-                        <p className="card-text text-center"><button className="btn btn-outline-danger" onClick={()=>changeAmount(amount-1)}>-</button><span>{amount}</span><button className="btn btn-outline-primary" onClick={()=>changeAmount(amount+1)}>+</button></p>
-                        <p className="card-text"><strong className="text-danger">Total: </strong> {props.price*amount}</p>
+                        <div className="d-flex justify-content-around align-items-baseline">
+                            <p className="card-text"><strong className="text-danger">Valor: </strong> {props.price}</p>
+                            <p className="card-text text-center"><button className="btn btn-outline-danger" onClick={() => changeAmount(amount - 1)}>-</button><span className="counter">{amount}</span><button className="btn btn-outline-primary" onClick={() => changeAmount(amount + 1)}>+</button></p>
+                            <p className="card-text"><strong className="text-danger">Total: </strong> {props.price * amount}</p>
+                        </div>
                     </div>
                     <div className="card-footer">
-                        <button className="btn btn-danger" onClick={()=>actions.deleteOrder(props.type, props.id)}>Quitar</button>
+                        <button className="btn btn-danger" onClick={() => actions.deleteOrder(props.type, props.id)}>Quitar</button>
                     </div>
                 </div>
             </div>
--- frontend/src/pages/order.js
+++ frontend/src/pages/order.js
@@ -8,92 +8,105 @@ import { OrderCard } from "../components/order_card";
 
 export const Order = () => {
 
-  const { store, actions } = useContext(Context);
-const [delivery, setDelivery] = useState(null);
-  var total = 0;
+    const { store, actions } = useContext(Context);
+    const [delivery, setDelivery] = useState(null);
+    var total = 0;
 
- const locationDelivery = [
-    {name: "Quilpué", price: 2000},
-    {name: "Villa Alemana", price: 2000},
-    {name: "Viña del Mar", price: 3000}
-  ];
+    const locationDelivery = [
+        { name: "Quilpué", price: 2000 },
+        { name: "Villa Alemana", price: 2000 },
+        { name: "Viña del Mar", price: 3000 }
+    ];
 
-  const handleDelivery = (e)=>{
-    let value = e.target.value;
-    console.log(value);
-    if (locationDelivery.map(loc=>loc.name).includes(value)){
-        setDelivery(locationDelivery.filter(loc=>loc.name==value)[0].price);
-    }else{
-        setDelivery(null);
-    }
-  };
-
-
-  return (
-    <div className="container-fluid">
-        <h1>Tu pedido</h1>
-        <div className="row row-cols-md-2 row-cols-1">
-        {(store.order.combos.length > 0 || store.order.rolls.length > 0)
-        ? <div className="col">
-            {store.order.rolls?.map((item, index)=>{
-                let aux = store.rolls?.filter(product=>product.id == item.id)[0];
-                return (
-                    <OrderCard
-                        key={index} 
-                        id={item.id}
-                        type="rolls"
-                        title={aux?.name}
-                        price={aux?.price}
-                        amount={item.amount}
-                        image={aux?.image}
-                    />
-                );
-            })}
-            {store.order.combos?.map((combo, index)=>{
-                return (
-                    <div key={index}>Combo</div>
-                );
-            })}
-        </div>
-        : (store.order.rolls.length == 0 && store.order.combos.length == 0)
-         ? <h2>No hay productos. Agregalos...</h2>
-         : <Spinners />
+    const handleDelivery = (e) => {
+        let value = e.target.value;
+        console.log(value);
+        if (locationDelivery.map(loc => loc.name).includes(value)) {
+            setDelivery(locationDelivery.filter(loc => loc.name == value)[0].price);
+        } else {
+            setDelivery(null);
         }
-        <div className="col bg-white text-center">
-            <h2>Detalles compra</h2>
-            <ol>
-            {store.order.rolls && store.order.rolls.map((item,index)=>{
-                {total=total+item.amount * store.rolls.filter(roll=>roll.id == item.id)[0]?.price;}
-                return <li key={index}>
-                    {store.rolls.filter(roll=>roll.id == item.id)[0]?.name} x {item.amount} = ${item.amount * store.rolls.filter(roll=>roll.id == item.id)[0]?.price}
-                </li>;
-            })}
-            {store.order.combos && store.order.combos.map((item,index)=>{
-                return <li key={index}>
-                    {store.combos.filter(combo=>combo.id == item.id)[0]?.name} x {item.amount} = ${item.amount * store.combos.filter(combo=>combo.id == item.id)[0]?.price}
-                </li>;
-            })}
-            </ol>
-            <div>
-                Delivery = {
-                total>=15000 
-                ? "Gratis!"
-                : !!delivery && "$"+delivery
-                }
-                <div>
-                    <input list="ciudades" name="ciudades" placeholder="Selecciona tu localidad" onChange={handleDelivery}/>
-                    <datalist id="ciudades">
-                        {locationDelivery.map((item, index)=>{
-                            return <option key={index} value={item.name}>{item.price}</option>;
+    };
+
+
+    return (
+        <div className="container">
+            <h1>Tu pedido</h1>
+            <div className="d-flex justify-content-evenly">
+
+                <div className="row row-cols-1">
+                    {(store.order.combos.length > 0 || store.order.rolls.length > 0)
+                        ? <div className="col">
+                            {store.order.rolls?.map((item, index) => {
+                                let aux = store.rolls?.filter(product => product.id == item.id)[0];
+                                return (
+                                    <OrderCard
+                                        key={index}
+                                        id={item.id}
+                                        type="rolls"
+                                        title={aux?.name}
+                                        price={aux?.price}
+                                        amount={item.amount}
+                                        image={aux?.image}
+                                    />
+                                );
+                            })}
+                            {store.order.combos?.map((item, index) => {
+                                let aux = store.combos?.filter(product => product.id == item.id)[0];
+                                return (
+                                    <OrderCard
+                                        key={index}
+                                        id={item.id}
+                                        type="rolls"
+                                        title={aux?.name}
+                                        price={aux?.price}
+                                        amount={item.amount}
+                                        image={aux?.image}
+                                    />
+                                );
+                            })}
+                        </div>
+                        : (store.order.rolls.length == 0 && store.order.combos.length == 0)
+                            ? <h2>No hay productos. Agregalos...</h2>
+                            : <Spinners />
+                    }
+                </div>
+                <div className="bg-white d-flex flex-column text-center align-items-stretch sticky-md-top" style={{height: "fit-content"}}>
+                    <h2>Detalles compra</h2>
+                    <ul className="list-group list-group-flush">
+                        {store.order.rolls && store.order.rolls.map((item, index) => {
+                            { total = total + item.amount * store.rolls.filter(roll => roll.id == item.id)[0]?.price; }
+                            return <li className="list-group-item d-flex justify-content-between" key={index}>
+                                {store.rolls.filter(roll => roll.id == item.id)[0]?.name} x {item.amount} <span className="ms-1">${item.amount * store.rolls.filter(roll => roll.id == item.id)[0]?.price}</span>
+                            </li>;
                         })}
-                    </datalist>
+                        {store.order.combos && store.order.combos.map((item, index) => {
+                            return <li className="list-group-item d-flex justify-content-between" key={index}>
+                                {store.combos.filter(combo => combo.id == item.id)[0]?.name} x {item.amount} <span className="ms-1"> ${item.amount * store.combos.filter(combo => combo.id == item.id)[0]?.price}</span>
+                            </li>;
+                        })}
+                    </ul>
+                    <div>
+                        Delivery = {
+                            total >= 15000
+                                ? "Gratis!"
+                                : !!delivery && "$" + delivery
+                        }
+                        <div>
+                            <input list="ciudades" name="ciudades" placeholder="Selecciona tu localidad" onChange={handleDelivery} />
+                            <datalist id="ciudades">
+                                {locationDelivery.map((item, index) => {
+                                    return <option key={index} value={item.name}>{item.price}</option>;
+                                })}
+                            </datalist>
+                        </div>
+                    </div>
+                    <p>TOTAL = ${total >= 15000 ? total : total + delivery}</p>
                 </div>
             </div>
-            <p>TOTAL = ${total >= 15000 ? total : total + delivery}</p>
+
         </div>
-        </div>
-        
-    </div>
-  );
+
+    );
 };
 
