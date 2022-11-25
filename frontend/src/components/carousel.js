361c13bf9bb1c611fec2098e4947846d66d663e2
Bryan Alburquenque <alburquenque.letelier@gmail.com>
2022-11-25 11:36:43 -0300

    Se ajusta carousels

--- frontend/src/components/carousel.js
+++ frontend/src/components/carousel.js
@@ -1,13 +1,13 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
-import PropTypes, { element } from "prop-types";
 
 import "../styles/carousel.css";
 
 export const Carousel = () => {
     const {store} = useContext(Context);
     // title={item.name} description={item.description} image={item.image} price={item.price}
-    
+
+
     return (
         <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="true">
             <div className="carousel-indicators">
@@ -16,9 +16,23 @@ export const Carousel = () => {
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
             </div>
             <div className="carousel-inner">
-                <div className="carousel-item active">
-                    <img 
-                    src={store?.carousels && store.rolls.filter(prod=>prod.id==store?.carousels[0]?.rolls_images)[0]?.image
+                {store.carousels &&
+                    store.carousels.map((item,index)=>{
+                      return <div key={index} className={index==0 ? "carousel-item active" : "carousel-item"}>
+                            {item.rolls_images 
+                                ? <img src={store?.rolls?.filter(roll=>roll.name==item.rolls_images)[0]?.image} alt={item.rolls_images} />
+                                : <img src={store?.combos?.filter(combo=>combo.name==item.combo_images)[0]?.image} alt={item.combo_images} />
+                            }
+                        </div>; 
+                    })
+                }
+                {/* <div className="carousel-item active">
+                    <img
+                    src={store?.carousels?.rolls_images
+                        ? store?.rolls?.filter(roll=>roll.name==store?.carousels[0]?.rolls_images)[0]?.image
+                        : store?.carousels?.combo_images
+                            ?  store?.combos?.filter(combo=>combo.name==store?.carousels[0]?.combo_images)[0]?.image
+                            : "https://dummyimage.com/700x800/000/fff"
                     } 
                     className="d-block w-100" 
                     alt="https://dummyimage.com/700x800/000/fff" />
@@ -28,7 +42,7 @@ export const Carousel = () => {
                 </div>
                 <div className="carousel-item">
                     <img src={store?.carousels && store.rolls.filter(prod=>prod.id==store?.carousels[2]?.rolls_images)[0]?.image} className="d-block w-100" alt="https://dummyimage.com/600x400/000/fff" />
-                </div>
+                </div> */}
             </div>
             <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
                 <span className="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -40,11 +54,4 @@ export const Carousel = () => {
             </button>
         </div>
     );
-};
-
-Carousel.propsTypes = {
-    title: PropTypes.string,
-    image: PropTypes.string,
-    price: PropTypes.number,
-    amount: PropTypes.number
 };
