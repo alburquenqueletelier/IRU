321ecea5ca3e37a405a9fecddfe887ba350eb5b8
Bryan Alburquenque <alburquenque.letelier@gmail.com>
2022-12-08 11:15:37 -0300

    Se ajusta footer al final de la pagina

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from 'react-router-dom';
 import "../styles/footer.css";
 
-// mt-auto
 export const Footer = () => {
-//   const { store } = useContext(Context);
+  let location = useLocation();
+  const [fixPosition, setFixPosition] = useState(false);
+  useEffect(() => {
+    console.log('location changed!');
+    if (window.innerHeight > document.querySelector('body').offsetHeight){
+      setFixPosition(true);
+    } else {
+      setFixPosition(false);
+    }
+  }, [location]);
+
   return (
-  <div id="footer" className="pb-1">
-    <h2 className="text-center">Siguenos en nuestras redes sociales</h2>
-    <div className="row justify-content-center">
-      <div className="col-auto">
-      <a href="https://www.instagram.com/irollyou_cinnamon/" rel="noreferrer" target="_blank"><img style={{width:"30px", margin:"auto"}} src="https://cdn-icons-png.flaticon.com/512/3955/3955024.png" alt="insta" /></a>
-      </div>
-      <div className="col-auto">
-      <a href="https://www.tiktok.com/@irollyou_cinnamon" rel="noreferrer" target="_blank"><img style={{width:"30px", margin:"auto"}} src="https://cdn-icons-png.flaticon.com/512/3670/3670132.png" alt="insta" /></a>
+    <div id="footer" className={fixPosition ? "pb-1 fixed-bottom" : "pb-1"}>
+      <h2 className="text-center">Siguenos en nuestras redes sociales</h2>
+      <div className="row justify-content-center">
+        <div className="col-auto">
+          <a href="https://www.instagram.com/irollyou_cinnamon/" rel="noreferrer" target="_blank"><img style={{ width: "30px", margin: "auto" }} src="https://cdn-icons-png.flaticon.com/512/3955/3955024.png" alt="insta" /></a>
+        </div>
+        <div className="col-auto">
+          <a href="https://www.tiktok.com/@irollyou_cinnamon" rel="noreferrer" target="_blank"><img style={{ width: "30px", margin: "auto" }} src="https://cdn-icons-png.flaticon.com/512/3670/3670132.png" alt="insta" /></a>
+        </div>
       </div>
     </div>
-  </div>
   );
 };
